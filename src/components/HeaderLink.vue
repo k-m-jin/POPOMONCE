7aeded5d323d8e5866cc78c32ae4e0ed2488325a
whilini <whi0306@naver.com>
2022-07-25 01:33:32 +0900

    fix: console error & router link url

--- src/components/HeaderLink.vue
+++ src/components/HeaderLink.vue
@@ -36,7 +36,7 @@
                     >내 계좌 정보 확인
                   </el-dropdown-item>
                   <el-dropdown-item divided class="drop-down">
-                    <router-link class="router-link" to="/userPaidInfo">
+                    <router-link class="router-link" to="/paidlist">
                       <el-icon class="el-icon--left">
                         <shopping-cart /> </el-icon
                       >내 구매 목록 확인
@@ -88,6 +88,7 @@
         return this.$store.state.user.profileImg;
       },
       isAdmin() {
+        console.log(this.$store.state.isAdmin);
         return this.$store.state.isAdmin;
       },
     },
