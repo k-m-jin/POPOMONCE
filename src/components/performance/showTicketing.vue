dbbed86ce805f320b56e255e671c4e014aefc49f
k-m-jin <mel917@naver.com>
2022-07-25 02:38:59 +0900

    Feat: Css showTicketing.vue

--- src/components/performance/showTicketing.vue
+++ src/components/performance/showTicketing.vue
@@ -5,17 +5,14 @@
         :range="[
           new Date(startDate[0], startDate[1] - 1, startDate[2]),
           new Date(endDate[0], endDate[1] - 1, endDate[2]),
-        ]"
-      />
+        ]" />
     </div>
-    <div class="price">가격 : {{ price }}</div>
-    <div class="date">
-      {{ startDate }}
+    <div class="info">
+      <div class="price">
+        가격 : {{ price }} 원
+      </div>
+      <el-button>결제하기</el-button>
     </div>
-    <div class="date">
-      {{ endDate }}
-    </div>
-    <el-button>결제하기</el-button>
   </el-card>
 </template>
 <script>
@@ -60,6 +57,11 @@
         height: 400px;
         --el-calendar-cell-width: 30px;
       }
+    }
+    .info{
+      display: flex;
+      align-items: center;
+      flex-direction: column;
     }
     .date {
       height: 2rem;
