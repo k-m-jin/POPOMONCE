0d69a5ab97992ae3b3236e4a04cdc0e2071c84af
whilini <whi0306@naver.com>
2022-07-28 15:35:01 +0900

    config: payment button position

--- src/routes/PaymentPage.vue
+++ src/routes/PaymentPage.vue
@@ -3,13 +3,9 @@
     <div class="pageWrap">
       <div class="bookingInfo">
         <h2>예매정보</h2>
-        <div
-          v-if="!pfLoading"
-          class="bookingInfo__details">
+        <div v-if="!pfLoading" class="bookingInfo__details">
           <div class="poster">
-            <img
-              :src="myChoice.mainPoster"
-              :alt="myChoice.title" />
+            <img :src="myChoice.mainPoster" :alt="myChoice.title" />
           </div>
           <div class="tags">
             <h3>{{ myChoice.title }}</h3>
@@ -18,7 +14,9 @@
             <p>공연장소 : {{ myChoice.concertHall }}</p>
             <h4>
               결제금액 :
-              <span :style="{ fontSize: 22 + 'px', fontWeight: 800 }">{{ myChoice.price }} 원</span>
+              <span :style="{ fontSize: 22 + 'px', fontWeight: 800 }"
+                >{{ myChoice.price }} 원</span
+              >
             </h4>
           </div>
         </div>
@@ -27,43 +25,30 @@
         <h2>결제수단</h2>
         <div class="payment__select">
           <h3>간편계좌이체</h3>
-          <div
-            v-if="!payLoading"
-            class="payment__select__bank">
+          <div v-if="!payLoading" class="payment__select__bank">
             <div
               v-for="bank in ableBankList"
               :key="bank.code"
               :ref="bank.id"
               class="bank-card"
               :class="{ noAccess: !bank.id }"
-              @click.stop="accountID(bank.id)">
+              @click.stop="accountID(bank.id)"
+            >
               <h4>{{ bank.name }}</h4>
-              <div
-                v-if="bank.disabled"
-                class="bank-detail">
+              <div v-if="bank.disabled" class="bank-detail">
                 <p>{{ bank.accountNumber }}</p>
                 <p>잔액: {{ bank.balance }} 원</p>
               </div>
             </div>
-            <div
-              class="bank-card new-bank"
-              @click="toggleModal">
+            <div class="bank-card new-bank" @click="toggleModal">
               <p><i class="fa-solid fa-plus"></i></p>
               신규계좌등록
             </div>
           </div>
-          <div v-else>
-            Loading...
-          </div>
+          <div v-else>Loading...</div>
         </div>
-        <div
-          class="payment__button"
-          @click="payPerformance">
-          결제하기
-        </div>
-        <Modal
-          :modal-active="modalActive"
-          @close="toggleModal" />
+        <div class="payment__button" @click="payPerformance">결제하기</div>
+        <Modal :modal-active="modalActive" @close="toggleModal" />
       </div>
     </div>
   </div>
@@ -202,7 +187,6 @@
     .pageWrap {
       max-width: 80%;
       width: 60%;
-      height: 850px;
       display: flex;
       position: relative;
       top: 25px;
@@ -337,6 +321,8 @@
           }
         }
         .payment__button {
+          position: absolute;
+          bottom: 0;
           width: 100%;
           height: 74px;
           background-color: #fe253f;
